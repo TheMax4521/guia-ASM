1edab37b65bca407e13fdac23359fc3c7cece354
dario <dariorossel@hotmail.com>
2025-05-02 19:58:52 -0300

    no entiendo

--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,19 +2,19 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
-LISTA_OFFSET_HEAD EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 9
+PACKED_NODO_OFFSET_LONGITUD EQU 17
+PACKED_NODO_SIZE EQU 21
+LISTA_OFFSET_HEAD EQU 0
 LISTA_SIZE EQU 8
-PACKED_LISTA_OFFSET_HEAD EQU 8
+PACKED_LISTA_OFFSET_HEAD EQU 0
 PACKED_LISTA_SIZE EQU 8
 
 ;########### SECCION DE DATOS
@@ -31,6 +31,27 @@ global cantidad_total_de_elementos_packed
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
 ;registros: lista[?]
 cantidad_total_de_elementos:
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14
+	mov rax, [rdi + LISTA_OFFSET_HEAD] ; rax  = nodo actual
+	xor ecx, ecx ; contrador total de elementos
+.bucle:
+	cmp rax, 0
+	je .fin
+	mov edx, dword [rax + NODO_OFFSET_LONGITUD]; edx = longitrud del arreglo actual
+	add ecx, edx
+	mov r12, [rax + NODO_OFFSET_NEXT]
+	mov rax, r12
+	jmp .bucle
+.fin:
+	mov eax, ecx
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
